2ba09a6dc3fb58bfeca8e3c79817ffeac4dd32dd
dincerti <devin.incerti@gmail.com>
2020-01-28 21:41:51 -0800

    Set up repository as R package with pkgdown website

---
+++ R/learn-cea.R
@@ -0,0 +1,7 @@
+#' learnCEA: Learn Cost-effectiveness Modeling with R
+#' 
+#' A companion package for the Professional Society for Health Economics 
+#' and Outcomes Research (ISPOR) short course "Applied cost-effectiveness modeling with R".
+#' @docType package
+#' @name learnCEA
+NULL
