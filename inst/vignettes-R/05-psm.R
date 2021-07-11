a7d92f8fba68656c27a802ec9fb83e848e718ec8
dincerti <devin.incerti@gmail.com>
2021-07-10 23:23:05 -0700

    Update with hesim >= 0.5.0

--- inst/vignettes-R/05-psm.R
+++ inst/vignettes-R/05-psm.R
@@ -5,6 +5,7 @@ library("hesim")
 library("data.table")
 library("ggplot2")
 library("flexsurv")
+theme_set(theme_bw())
 
 set.seed(101) # Make PSA reproducible
 
@@ -34,9 +35,16 @@ hesim_dat <- hesim_data(
 )
 print(hesim_dat)
 
+## @knitr labels
+labs <- get_labels(hesim_dat)
+
 ## ---- Parameter estimation ---------------------------------------------------
-## @knitr sim_pfs_os_data
-surv_est_data <- rcea::sim_pfs_os_data(n = 2000)
+## @knitr pfs_os_data
+onc3 <- hesim::onc3[strategy_name != "New 1"]
+onc3[, strategy_name := droplevels(strategy_name)]
+levels(onc3$strategy_name) <- c("SOC", "New")
+surv_est_data <- as_pfs_os(onc3, patient_vars = c("patient_id", "female", "age",
+                                                  "strategy_name"))
 surv_est_data[patient_id %in% c(1, 2)]
 
 ## @knitr fit-survival-models
@@ -59,8 +67,8 @@ utility_tbl <- stateval_tbl(
              mean = c(.8, S = .6),
              se = c(0.02, .05)
   ),
-  dist = "beta",
-  hesim_data = hesim_dat)
+  dist = "beta"
+)
 print(utility_tbl)
 
 # Medical costs
@@ -69,16 +77,16 @@ medcost_tbl <- stateval_tbl(
              mean = c(2000, 9500),
              se = c(2000, 9500)
   ),
-  dist = "gamma",
-  hesim_data = hesim_dat)
+  dist = "gamma"
+)
 print(medcost_tbl)
 
 ## @knitr drugcost_tbl
 drugcost_tbl <- stateval_tbl(
   data.table(strategy_id = strategies$strategy_id,
              est = c(2000, 12000)),
-  dist = "fixed",
-  hesim_data = hesim_dat)
+  dist = "fixed"
+)
 print(drugcost_tbl)
 
 ## ---- Simulation -------------------------------------------------------------
@@ -95,15 +103,19 @@ print(survmods_data)
 survmods <- create_PsmCurves(psfit_wei, 
                              input_data = survmods_data, 
                              n = n_samples,
-                             bootstrap = TRUE, est_data = surv_est_data)
+                             uncertainty = "bootstrap", 
+                             est_data = surv_est_data)
 
 ## @knitr utility-cost-models 
 # Utility
-utilitymod <- create_StateVals(utility_tbl, n = n_samples)
+utilitymod <- create_StateVals(utility_tbl, n = n_samples,
+                               hesim_data = hesim_dat)
 
 # Costs
-drugcostmod <- create_StateVals(drugcost_tbl, n = n_samples)
-medcostmod <- create_StateVals(medcost_tbl, n = n_samples)
+drugcostmod <- create_StateVals(drugcost_tbl, n = n_samples,
+                                hesim_data = hesim_dat)
+medcostmod <- create_StateVals(medcost_tbl, n = n_samples,
+                               hesim_data = hesim_dat)
 costmods <- list(Drug = drugcostmod,
                  Medical = medcostmod)
 
@@ -114,9 +126,10 @@ econmod <- Psm$new(survival_models = survmods,
 
 ## ---- Simulate outcomes --- ##
 ## @knitr sim_survival
-times <- seq(0, 20, by = .1)
+times <- seq(0, 50, by = .1)
 econmod$sim_survival(t = times)
-head(econmod$survival_)
+autoplot(econmod$survival_, ci = TRUE,
+         labels = labs)
 
 ## @knitr sim_stateprobs
 econmod$sim_stateprobs()
@@ -130,4 +143,4 @@ econmod$sim_qalys(dr = .03)
 ## @knitr icer
 ce_sim <- econmod$summarize()
 cea_pw_out <- cea_pw(ce_sim, comparator = 1, dr_qalys = .03, dr_costs = .03)
-icer_tbl(cea_pw_out, colnames = strategies$strategy_name)
+format(icer(cea_pw_out, labels = labs))
