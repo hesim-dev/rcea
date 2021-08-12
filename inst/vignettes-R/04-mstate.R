545a37d07da3b6bae80d99a6953afaf839a38124
dincerti <devin.incerti@gmail.com>
2021-08-11 22:09:57 -0700

    Minor edits to mstate vignette

--- inst/vignettes-R/04-mstate.R
+++ inst/vignettes-R/04-mstate.R
@@ -64,7 +64,7 @@ for (i in 1:length(wei_fits)){
     Surv(time, status) ~ strategy_name + female,
     data = data,
     subset = (transition_id == i) ,
-    dist = "gengamma")
+    dist = "weibull")
 }
 wei_fits <- flexsurvreg_list(wei_fits)
 
@@ -116,7 +116,7 @@ head(transmod_data)
 ## @knitr transition-model
 transmod <- create_IndivCtstmTrans(wei_fits, transmod_data,
                                    trans_mat = tmat, n = n_samples,
-                                   clock = "forward",
+                                   clock = "reset",
                                    start_age = patients$age)
 
 ## @knitr utility-cost-models
